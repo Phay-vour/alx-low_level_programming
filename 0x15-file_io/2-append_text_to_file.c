417c76365164615d07454a6f0dd94f4033b10cfd
phay-vour <favourhachimenum@gmail.com>
2023-10-02 13:49:34 -0700

    append_text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * append_text_to_file - append text at the end of a file.
+ * append_text_to_file - Appends text at the end of a file.
  * @filename: A pointer to the name of the file.
  * @text_content: The string to add to the end of the file.
  *
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int i, s, len = 0;
+	int o, q, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,13 +22,13 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	i = open(filename, I_WRONLY | I_APPEND);
-	s = write(i, text_content, len);
+	o = open(filename, O_WRONLY | O_APPEND);
+	q = write(o, text_content, len);
 
-	if (i == -1 || s== -1)
+	if (o == -1 || q == -1)
 		return (-1);
 
-	close(i);
+	close(o);
 
 	return (1);
 }
